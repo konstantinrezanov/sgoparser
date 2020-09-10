aeede5bbe9f2ccb5331f0e1380dc9ffcba467cb7
Petrucciantor3000 <kostarezanov@gmail.com>
2020-09-10 18:12:03 +0500

    Final update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ system('rm *.xlsx && rm ~/Downloads/*.xlsx')
 
 check_title=get_xlsx(check_title,YOUR_LOGIN, YOUR_PASSWORD)
 print ('Sleeping')
-sleep(10)
+sleep(300)
 while True:
     check_title=get_xlsx(check_title,YOUR_LOGIN, YOUR_PASSWORD)
-    sleep(10)
+    sleep(300)
