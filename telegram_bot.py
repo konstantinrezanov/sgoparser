0959d943a8342e394e53727db4006c39540659ab
Petrucciantor3000 <kostarezanov@gmail.com>
2020-09-11 01:15:36 +0500

    Added all other classes

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,6 +10,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Стильный, модный, молодежный парсер на питоне")
 
+def showa(update, context):
+    f=open('raspa.txt')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f.read())
+
 def showb(update, context):
     f=open('raspb.txt')
     context.bot.send_message(chat_id=update.effective_chat.id, text=f.read())
@@ -18,14 +22,31 @@ def showv(update, context):
     f=open('raspv.txt')
     context.bot.send_message(chat_id=update.effective_chat.id, text=f.read())
 
+def showg(update, context):
+    f=open('raspg.txt')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f.read())
+
+def showd(update, context):
+    f=open('raspd.txt')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f.read())
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+showa_handler = CommandHandler('showa', showa)
+dispatcher.add_handler(showa_handler)
 
 showb_handler = CommandHandler('showb', showb)
 dispatcher.add_handler(showb_handler)
 
 showv_handler = CommandHandler('showv', showv)
 dispatcher.add_handler(showv_handler)
+
+showg_handler = CommandHandler('showg', showg)
+dispatcher.add_handler(showg_handler)
+
+showd_handler = CommandHandler('showd', showd)
+dispatcher.add_handler(showd_handler)
 
 updater.start_polling()
 updater.idle()
