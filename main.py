297cf68b36e2e8d38e1dd22fe99b6066cd935eda
petruccinator3000 <66559535+petruccinator3000@users.noreply.github.com>
2020-09-10 06:12:12 +0500

    Updated sleep times to a realistic values

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ mass=get_xlsx(check_title,YOUR_LOGIN, YOUR_PASSWORD)
 check_title=mass[0]
 print ('Sleeping')
 print(mass[1])
-sleep(10)
+sleep(300)
 while True:
     mass=get_xlsx(check_title,YOUR_LOGIN, YOUR_PASSWORD)
     check_title=mass[0]
     print(mass[1])
-    sleep(10)
+    sleep(300)
