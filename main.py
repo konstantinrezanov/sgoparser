926a9773c7b7a4da4f0de2592ada4163910e0e07
Petrucciantor3000 <kostarezanov@gmail.com>
2020-09-11 17:44:06 +0500

    update

--- main.py
+++ main.py
@@ -3,23 +3,32 @@ from parser import get_xlsx
 from os import system
 from check_time import check_time
 from waiting import wait
+from datetime import datetime
 
 while True: #restarts script in the case of an error
+    if datetime.now().hour>=16:
+        tim=7200
+    else:
+        tim=600
     check_title='init'
     system('rm *.xlsx && rm ~/Downloads/*.xlsx')
 
     check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
     print ('Sleeping')
-    sleep(600)
+    sleep(tim)
 
     wait(lambda: check_time())
 
     print('Done waiting')
 
     while True:
+        if datetime.now().hour>=16:
+            tim=7200
+        else:
+            tim=600
         while check_time():
             check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
-            sleep(600)
+            sleep(tim)
         wait(lambda: check_time())
 
         print('Done waiting')
