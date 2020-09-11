c9601b341051ffe4edf93cb71126109b1e65bec8
Petrucciantor3000 <kostarezanov@gmail.com>
2020-09-11 16:03:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from os import system
 from check_time import check_time
 from waiting import wait
 
-check_title='init'
-system('rm *.xlsx && rm ~/Downloads/*.xlsx')
+while True: #restarts script in the case of an error
+    check_title='init'
+    system('rm *.xlsx && rm ~/Downloads/*.xlsx')
 
-check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
-print ('Sleeping')
-sleep(600)
+    check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
+    print ('Sleeping')
+    sleep(600)
 
-wait(lambda: check_time())
-
-print('Done waiting')
-
-while True:
-    while check_time():
-        check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
-        sleep(600)
     wait(lambda: check_time())
 
     print('Done waiting')
+
+    while True:
+        while check_time():
+            check_title=get_xlsx(check_title,open('login.txt', 'r').read(), open('pass.txt', 'r').read())
+            sleep(600)
+        wait(lambda: check_time())
+
+        print('Done waiting')
